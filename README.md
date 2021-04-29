6e7a49c4b5dfb87067c684735091aee57113d189
Chris McColgan <mccolganchrisj@gmail.com>
2021-04-29 16:55:19 -0500

    forwardRef

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Chapter10
 
-React practice with useEffect, debounce, useReducer, and useContext
+More React practice. Concepts covered include debounce, useEffect, useReducer, useContext, useImperativeHandle, forwardRef, and useRef.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './store/auth-context';
+
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const ctx = useContext(AuthContext);
